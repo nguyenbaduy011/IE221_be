93b81031396893954254e5c594da64fff2f03629
giakhang0812 <phamnguyengiakhang14@gmail.com>
2025-10-12 14:25:19 +0700

    feat: add API to retrieve course detail by ID

--- course/urls.py
+++ course/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CourseListView
+from .views import *
 
 urlpatterns = [
     path('courses/', CourseListView.as_view(), name='course-list'), # Đường dẫn để lấy danh sách khóa học
+    path('courses/detail/', CourseDetailView.as_view(), name='course-detail'), # Đường dẫn để lấy chi tiết khóa học theo ID
 ]
