990b75d3f2cc280f46317f34b1babe71923867ea
giakhang0812 <phamnguyengiakhang14@gmail.com>
2025-10-13 20:02:21 +0700

    feat: add API to search courses by name and creator

--- course/selectors.py
+++ course/selectors.py
@@ -10,3 +10,14 @@ def get_course_by_id(course_id) -> Courses | None:
         return Courses.objects.get(id=course_id)
     except Courses.DoesNotExist:
         return None
+
+# Lấy khóa học theo tên, người tạo
+def get_courses_by_name_and_creator(name: str, creator_id: int) -> list[Courses]:
+    queryset = Courses.objects.all()
+
+    if name:
+        queryset = queryset.filter(name__icontains=name)
+    if creator_id:
+        queryset = queryset.filter(creator_id=creator_id)
+
+    return queryset
--- course/urls.py
+++ course/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('courses/', CourseListView.as_view(), name='course-list'), # Đường dẫn để lấy danh sách khóa học
     path('courses/detail/', CourseDetailView.as_view(), name='course-detail'), # Đường dẫn để lấy chi tiết khóa học theo ID
+    path('courses/search/', CourseSearchView.as_view(), name='course-search'), # Đường dẫn để tìm kiếm khóa học theo tên và người tạo
 ]
