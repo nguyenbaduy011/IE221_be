98a4250843833b3a105d1c5d801af76f185dfa54
letungbaoan <baoan7604@gmail.com>
2025-11-17 22:10:02 +0700

    Fix daily report

--- daily_reports/views/trainee_views.py
+++ daily_reports/views/trainee_views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 from django.utils import timezone
 from ..models import DailyReport
 from ..serializers import DailyReportSerializer
+
 
 class TraineeDailyReportViewSet(viewsets.ModelViewSet):
     """
@@ -18,9 +20,23 @@ class TraineeDailyReportViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
+        token_payload = self.request.auth
+
+        # Lấy user_id từ token
+        token_user_id = token_payload.get("user_id")
+        if not token_user_id:
+            raise PermissionDenied("Invalid token: missing user_id")
+
+        # Kiểm tra role trainee
+        if not hasattr(user, "role") or user.role != "trainee":
+            raise PermissionDenied("You are not allowed to access trainee reports.")
+
+        # Chỉ lấy báo cáo của chính user
         queryset = DailyReport.objects.filter(user=user)
-        course_id = self.request.query_params.get('course_id')
-        filter_date = self.request.query_params.get('filter_date')
+
+        # Filter thêm nếu có
+        course_id = self.request.query_params.get("course_id")
+        filter_date = self.request.query_params.get("filter_date")
 
         if course_id:
             queryset = queryset.filter(course_id=course_id)
@@ -30,4 +46,5 @@ class TraineeDailyReportViewSet(viewsets.ModelViewSet):
         return queryset
 
     def perform_create(self, serializer):
+        # Tự gán user từ token
         serializer.save(user=self.request.user)
