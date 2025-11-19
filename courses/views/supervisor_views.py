b1c0b957c5a286f260d48c93d80646f0d4a07bf2
giakhang0812 <phamnguyengiakhang14@gmail.com>
2025-11-19 10:41:21 +0700

    feat: add create course api

--- courses/views/supervisor_views.py
+++ courses/views/supervisor_views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from courses.models.course_model import Course
-from courses.serializers.course_serializer import CourseSerializer
+from courses.serializers.course_serializer import CourseSerializer, CourseCreateSerializer
 from courses.selectors import get_all_courses, get_course_by_id
 
 class IsSupervisor(permissions.BasePermission):
@@ -37,8 +37,22 @@ class SupervisorCourseDetailView(APIView):
         if not course:
             return Response({"detail": "Course not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        if getattr(user, 'role', None) != 'admin' and not course.course_supervisors.filter(supervisor=user).exists():
+        if getattr(user, 'role', None) != 'ADMIN' and not course.course_supervisors.filter(supervisor=user).exists():
             return Response({"detail": "You do not have permission to view this course."}, status=status.HTTP_403_FORBIDDEN)
 
         serializer = self.serializer_class(course)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class SupervisorCourseCreateView(APIView):
+    serializer_class = CourseCreateSerializer
+    permission_classes = [permissions.IsAuthenticated, IsSupervisor]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        course = CourseCreateService.create_course(user=request.user, validated_data=serializer.validated_data)
+        response_serializer = self.serializer_class(course)
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+
